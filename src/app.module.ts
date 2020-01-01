37ad0678ff75d7ee4d18e08b6d8ae6ffd7792799
VladimirJV <ivladimirjuarez@outlook.com>
2020-01-01 09:59:12 -0600

    User Entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SharedModule } from './shared/shared.module';
 import { ConfigService } from './shared/services/config.service';
+import { UserModule } from './api/user/user.module';
+import { AuthModule } from './api/auth/auth.module';
 
 @Module({
     imports: [
@@ -13,6 +15,8 @@ import { ConfigService } from './shared/services/config.service';
                 configService.typeOrmConfig,
             inject: [ConfigService],
         }),
+        UserModule,
+        AuthModule,
     ],
     controllers: [AppController],
     providers: [AppService],
